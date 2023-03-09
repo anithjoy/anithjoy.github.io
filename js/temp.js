0b4df01adb25612d5c7fdddc174d079c96cff0b0
anithjoy <anithjoy1@gmail.com>
2023-03-09 11:46:01 -0500

    3/9/2023

---
+++ js/temp.js
@@ -0,0 +1,8 @@
+const Hello = () => {
+    return (<div>
+        <h1> Hey fans of anith Joy(lol). PortFolio Coming soon</h1>
+        <p>Work in progress</p>
+        <p>Running this in reactJS fucntion class</p>
+        
+    </div>)
+}
